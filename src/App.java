86a41fa3c62701c93a4b4abc8b7bd7ca2e53718f
Guti <gutigb92@gmail.com>
2025-03-26 17:25:51 +0100

    menu

--- src/App.java
+++ src/App.java
@@ -1,39 +1,44 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
+    private static Scanner sc = new Scanner(System.in);
+    private static File carpetaSeleccionada;
+    private static List<LinkedHashMap<String, String>> datosArchivo = new ArrayList<>();
+
+    public static void main(String[] args) {
         int opcion;
-        
+
         do {
             System.out.println("Menú:");
             System.out.println("1. Seleccionar carpeta");
             System.out.println("2. Lectura de fichero");
-            System.out.println("3. Conversor");
-            System.out.println("4. Salir del programa");
+            System.out.println("3. Conversión a (csv, json, xml)");
+            System.out.println("4. Salir");
             System.out.print("Seleccione una opción: ");
+
             opcion = sc.nextInt();
             sc.nextLine(); 
-            
+
             switch (opcion) {
                 case 1:
+                    
                     break;
-                case 2:                   
-                    break;                
-                case 3:                    
+                case 2:
+                    
+                    break;
+                case 3:
+                
                     break;
                 case 4:
-                    System.out.println("Saliendo");
+                    System.out.println("Saliendo del programa.");
                     break;
-                
                 default:
-                    System.out.println("Opción no valida.");
-                    break;
+                    System.out.println("Opción no válida. Intente de nuevo.");
             }
         } while (opcion != 4);
 
         sc.close();
     }
-
 
 }
